e33222b162deaa4c1a059a40369defdffeb97743
luisangeluis <luisangeluis@gmail.com>
2022-07-07 13:33:34 -0500

    Styles to cards

--- src/components/products/CardProduct.jsx
+++ src/components/products/CardProduct.jsx
@@ -12,20 +12,26 @@ const CardProduct = ({ product }) => {
     <div className="col-md-6 col-lg-4 my-2 my-md-3">
       <div className="card products-list_card d-flex justify-content-center align-items-center flex-column py-2 py-md-3">
         <button
-          className="btn h-100 w-100"
+          className="btn h-100 w-100 d-flex flex-column"
           onClick={() => goToDetail(product.id)}
         >
-          <img src={product.productImgs[0]} className="img-fluid" alt="" />
-          <hr />
-          <div className="card-body">
+          <img
+            src={product.productImgs[0]}
+            className="img-fluid mx-auto p-2 p-md-3"
+            alt=""
+          />
+          <hr className="bg-dark w-100" />
+          <div className="card-body d-flex flex-column justify-content-between align-items-stretch w-75 mx-auto">
             <h5 className="card-title">{product.title}</h5>
-            <div className="d-flex justify-content-around align-items-end">
+            <div className="d-flex justify-content-between align-items-end">
               <div>
                 <p className="card-text m-0">Price</p>
-                <p className="card-text m-0">Price</p>
+                <p className="card-text m-0">${product.price}</p>
               </div>
               <div>
-                <p>hola</p>
+                <button className="btn btn-add-cart rounded-circle bg-orange">
+                  <i className="fa-solid fa-cart-shopping text-white"></i>
+                </button>
               </div>
             </div>
           </div>
